eb7333710bcacd73f17c78141c6ce3ee5b11d403
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-11-25 22:53:40 +0530

    fix: bolder fonts in section headers

--- frontend/src/components/SectionContainer.vue
+++ frontend/src/components/SectionContainer.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="mb-4 flex flex-col gap-3">
-		<div class="flex items-center justify-between text-sm">
-			<h3 class="text-base text-ink-gray-9">{{ title }}</h3>
+		<div class="flex items-center justify-between">
+			<h3 class="dark:text-zinc-300 text-base font-medium text-ink-gray-9">{{ title }}</h3>
 			<slot name="actions"></slot>
 		</div>
 		<slot></slot>
