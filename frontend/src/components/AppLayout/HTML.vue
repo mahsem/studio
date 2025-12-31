7f6339dffc995948ae38599c3dae165a013d4556
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-12-31 18:36:00 +0530

    fix: use compile function to compile template before display

--- frontend/src/components/AppLayout/HTML.vue
+++ frontend/src/components/AppLayout/HTML.vue
@@ -1,20 +1,14 @@
 <template>
-	<div ref="component" class="!relative" v-html="HTML"></div>
+	<component :is="HTML"></component>
 </template>
 <script setup lang="ts">
-import { computed, ref } from "vue"
+import { computed, ref, compile } from "vue"
 import type { HTMLProps } from "@/types/studio_components/HTML"
 
 const component = ref<HTMLElement | null>(null)
 const props = defineProps<HTMLProps>()
 
-const HTML = computed(() => {
-	let content = props.html
-	return `
-		<div class="absolute top-0 bottom-0 right-0 left-0"></div>
-		${content}
-	`
-})
+const HTML = computed(() => compile(props.html))
 
 defineExpose({
 	component,
