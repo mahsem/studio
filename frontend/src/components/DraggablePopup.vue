9ec93f1d34661c7b06d81555ae2b0312d1b55288
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-01-15 12:02:50 +0530

    fix: Transport popup to popovers instead of body

--- frontend/src/components/DraggablePopup.vue
+++ frontend/src/components/DraggablePopup.vue
@@ -1,8 +1,8 @@
 <!-- Extracted from Builder -->
 <template>
-	<teleport to="body">
+	<teleport to="#popovers">
 		<div class="relative" ref="popover">
-			<div class="fixed z-50" @mousedown.stop>
+			<div class="fixed" @mousedown.stop>
 				<div
 					ref="popoverContent"
 					class="fixed flex flex-col gap-1 overflow-hidden rounded-lg border border-outline-gray-2 bg-surface-white shadow-2xl"
