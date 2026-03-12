c92e86495d740a8adfe6dae755e3859ff27d1f33
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-03-12 14:13:46 +0530

    build: enable vue due tools in prod for app builder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
 	define: {
 		__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+		__VUE_PROD_DEVTOOLS__: true,
 	},
 	server: {
 		// explicitly set origin of generated assets (images, fonts, etc) during development.
