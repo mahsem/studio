601bee89997f5305747c7b4100cf61d63cffdee9
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-03-16 14:44:10 +0530

    fix: move remove btn for ArrayInput to top right corner

--- frontend/src/components/ArrayInput.vue
+++ frontend/src/components/ArrayInput.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex h-full w-full flex-col gap-2.5">
+	<div class="flex h-full w-full flex-col gap-3">
 		<InputLabel
 			v-if="label"
 			:class="[required ? `after:text-red-600 after:content-['_*']` : '']"
@@ -8,7 +8,11 @@
 			{{ label }}
 		</InputLabel>
 
-		<div v-for="(item, index) in items" :key="index" class="group flex flex-col gap-2 rounded-md border p-3">
+		<div
+			v-for="(item, index) in items"
+			:key="index"
+			class="group/item relative flex flex-col gap-1.5 rounded-md border p-3"
+		>
 			<div
 				v-for="(fieldSchema, fieldKey) in itemTypes"
 				:key="fieldKey"
@@ -33,10 +37,15 @@
 					class="flex-1"
 				/>
 			</div>
-			<Button variant="outline" icon="x" class="w-full" @click="removeItem(index)" />
+			<div
+				title="Remove"
+				class="absolute right-0 top-0 hidden -translate-y-1/2 translate-x-1/2 cursor-pointer rounded-full border border-outline-gray-2 bg-white p-0.5 hover:bg-surface-gray-1 group-hover/item:block"
+			>
+				<FeatherIcon name="x" @click="removeItem(index)" class="size-3 rounded-full" />
+			</div>
 		</div>
 
-		<Button variant="outline" class="mt-1 w-full" icon-left="plus" @click="addItem">Add</Button>
+		<Button variant="outline" class="w-full" icon-left="plus" @click="addItem">Add</Button>
 	</div>
 </template>
 
