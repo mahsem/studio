5750f09388e1d1aa86d0ede26295586cc23153b3
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-04-15 17:17:43 +0530

    fix: patch router.resolve to resolve unmatched routes to not found page
    - This ensures router-link renders correctly for the app being edited in studio

--- frontend/src/pages/NotFound.vue
+++ frontend/src/pages/NotFound.vue
@@ -1,10 +1,8 @@
 <template>
 	<div class="flex h-screen w-full items-center justify-center bg-gray-50">
 		<div class="flex flex-col items-center justify-center">
-			<h1 class="mt-1 text-2xl font-semibold text-gray-600">403: Permission Denied</h1>
-			<div class="mt-2 text-base text-gray-500">
-				You don't have permission to access this page. Make sure you have the right roles & permissions.
-			</div>
+			<h1 class="mt-1 text-2xl font-semibold text-gray-600">404</h1>
+			<div class="mt-2 text-base text-gray-500">The page you are looking for does not exist.</div>
 			<a href="/desk" class="mt-4 text-base text-gray-600 underline">Back to Home</a>
 		</div>
 	</div>
