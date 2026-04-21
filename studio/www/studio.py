c6cb75de82cc374e74a1df21c779342853f32de8
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-04-21 16:57:00 +0530

    fix: developer mode flag

--- studio/www/studio.py
+++ studio/www/studio.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils import cint
 
 no_cache = 1
 
@@ -8,14 +9,14 @@ def get_context(context):
 	frappe.db.commit()
 	context.csrf_token = csrf_token
 	context.site_url = get_site_url()
-	context.is_developer_mode = frappe.conf.developer_mode
+	context.is_developer_mode = cint(frappe.conf.developer_mode)
 
 
 @frappe.whitelist(methods=["POST"], allow_guest=True)
 def get_context_for_dev():
 	if not frappe.conf.developer_mode:
 		frappe.throw(frappe._("This method is only meant for developer mode"))
-	return frappe._dict({"site_url": get_site_url(), "is_developer_mode": frappe.conf.developer_mode})
+	return frappe._dict({"site_url": get_site_url(), "is_developer_mode": cint(frappe.conf.developer_mode)})
 
 
 def get_site_url() -> str:
