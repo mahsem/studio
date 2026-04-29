d79b141da207f0da38980011158024ce288ad46f
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-04-29 12:51:54 +0530

    feat: add frappe-ui call to global utilities

--- frontend/src/utils/globalUtils.ts
+++ frontend/src/utils/globalUtils.ts
@@ -1,9 +1,9 @@
 import { h } from "vue"
-import { toast } from "frappe-ui"
+import { toast, call } from "frappe-ui"
 import { Icon } from "frappe-ui/icons"
 
 function getIcon(name: string) {
 	return h(Icon, { name })
 }
 
-export { getIcon, toast }
+export { getIcon, toast, call }
