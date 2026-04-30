28d6cdceaa3a0da911590f72b11ea9c849bdac8b
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-04-30 14:09:28 +0530

    fix: remove server vite config from app builds

--- frontend/src/scripts/build.js
+++ frontend/src/scripts/build.js
@@ -127,13 +127,6 @@ async function buildWithVite(appName, entryFilePath, outDir, basePath) {
 	await build({
 		root: path.resolve(__dirname, "../"),
 		base: basePath,
-		server: {
-			// explicitly set origin of generated assets (images, fonts, etc) during development.
-			// Required for the app renderer running on webserver port
-			// https://vite.dev/guide/backend-integration
-			origin: "http://127.0.0.1:8080",
-			allowedHosts: true,
-		},
 		plugins: [
 			vue(),
 			frappeui({
